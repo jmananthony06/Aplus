bb5b9c72344a5f9316099900d1e244ae24a9f523
jmananthony06 <jmananthony06@gmail.com>
2020-08-30 14:33:42 -0400

    mobile query added - more work needed
    still needs service paghe content from client (waiting) - also make services page mobile version - needs animations on both queries. might do tablet query also. where is hamburger icon?! find out how to do later but code is in the file it just doesn't show on page.

---
+++ js/Aplus.js
@@ -0,0 +1,10 @@
+// Mobile Query
+
+// function myFunction() {
+//     var x = document.getElementsByClassName("hamburgerMenu");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
